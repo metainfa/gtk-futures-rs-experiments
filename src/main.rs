1fa8254e61e4d28585a9bc95536fd7600746a1a9
Antoni Boucher <bouanto@zoho.com>
2017-02-06 21:39:58 -0500

    Switched to a stream.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate tokio_timer;
 
 use std::time::Duration;
 
-use futures::future::Future;
+use futures::{Future, Stream};
 use gtk::{Inhibit, WidgetExt, Window, WindowType};
 use tokio_core::reactor::Core;
 use tokio_timer::Timer;
@@ -23,12 +23,17 @@ fn main() {
 
     let mut core = Core::new().unwrap();
     let timer = Timer::default();
-    let sleep = timer.sleep(Duration::from_millis(5000));
+    let interval = timer.interval(Duration::from_millis(1000));
+    let future = interval.for_each(|_| {
+        println!("Interval");
+        Ok(())
+    });
+    /*let sleep = timer.sleep(Duration::from_millis(5000));
     let future = sleep.and_then(|_| {
         println!("Time out");
         let timer = Timer::default();
         timer.sleep(Duration::from_millis(0))
-    });
+    });*/
     let handle = core.handle();
     handle.spawn(future.map_err(|_| ()));
     loop {
