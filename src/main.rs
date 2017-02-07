3be3cb9970e07b4ec4c34f276a1ae782031de0da
Antoni Boucher <bouanto@zoho.com>
2017-02-06 21:35:28 -0500

    Gtk+ main loop + tokio event loop with a future running.

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+extern crate futures;
+extern crate gtk;
+extern crate tokio_core;
+extern crate tokio_timer;
+
+use std::time::Duration;
+
+use futures::future::Future;
+use gtk::{Inhibit, WidgetExt, Window, WindowType};
+use tokio_core::reactor::Core;
+use tokio_timer::Timer;
+
+fn main() {
+    gtk::init().unwrap();
+    let window = Window::new(WindowType::Toplevel);
+
+    window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    let mut core = Core::new().unwrap();
+    let timer = Timer::default();
+    let sleep = timer.sleep(Duration::from_millis(5000));
+    let future = sleep.and_then(|_| {
+        println!("Time out");
+        let timer = Timer::default();
+        timer.sleep(Duration::from_millis(0))
+    });
+    let handle = core.handle();
+    handle.spawn(future.map_err(|_| ()));
+    loop {
+        core.turn(Some(Duration::from_millis(10)));
+
+        if gtk::events_pending() {
+            gtk::main_iteration();
+        }
+
+    }
+    //core.run(sleep).unwrap();
+}
