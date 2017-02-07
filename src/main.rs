41bcee12d64c24d374c269e593d64de3b48719a1
Antoni Boucher <bouanto@zoho.com>
2017-02-06 22:48:50 -0500

    Added an event stream.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,107 @@
+extern crate crossbeam;
 extern crate futures;
 extern crate gtk;
 extern crate tokio_core;
 extern crate tokio_timer;
 
+use std::rc::Rc;
 use std::time::Duration;
 
-use futures::{Future, Stream};
-use gtk::{Inhibit, WidgetExt, Window, WindowType};
+use crossbeam::sync::MsQueue;
+use futures::{Async, Future, Poll, Stream};
+use gtk::{Button, ButtonExt, ContainerExt, Inhibit, WidgetExt, Window, WindowType};
 use tokio_core::reactor::Core;
 use tokio_timer::Timer;
 
+use self::Msg::*;
+
+#[derive(Clone, Debug)]
+enum Msg {
+    Clicked,
+    Quit,
+}
+
+#[derive(Clone)]
+struct EventStream<T> {
+    events: Rc<MsQueue<T>>,
+}
+
+impl<T> EventStream<T> {
+    fn new() -> Self {
+        EventStream {
+            events: Rc::new(MsQueue::new()),
+        }
+    }
+
+    fn emit(&self, event: T) {
+        self.events.push(event);
+    }
+
+    fn get_event(&self) -> Option<T> {
+        self.events.try_pop()
+    }
+}
+
+impl<T> Stream for EventStream<T> {
+    type Item = T;
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        match self.events.try_pop() {
+            Some(event) => Ok(Async::Ready(Some(event))),
+            None => Ok(Async::NotReady),
+        }
+    }
+}
+
 fn main() {
     gtk::init().unwrap();
+
+    let stream = EventStream::new();
+
     let window = Window::new(WindowType::Toplevel);
+
+    let button = Button::new_with_label("Click me!");
+    {
+        let stream = stream.clone();
+        button.connect_clicked(move |_| {
+            stream.emit(Clicked);
+        });
+    }
+    window.add(&button);
 
     window.show_all();
 
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
+    {
+        let stream = stream.clone();
+        window.connect_delete_event(move |_, _| {
+            stream.emit(Quit);
+            Inhibit(false)
+        });
+    }
 
     let mut core = Core::new().unwrap();
     let timer = Timer::default();
     let interval = timer.interval(Duration::from_millis(1000));
-    let future = interval.for_each(|_| {
+    let interval = interval.map_err(|_| ()).for_each(|_| {
         println!("Interval");
         Ok(())
     });
-    /*let sleep = timer.sleep(Duration::from_millis(5000));
-    let future = sleep.and_then(|_| {
-        println!("Time out");
-        let timer = Timer::default();
-        timer.sleep(Duration::from_millis(0))
-    });*/
+
+    let event_future = stream.for_each(|event| {
+        println!("{:?}", event);
+        Ok(())
+    });
+
+    let future = interval.join(event_future).map(|_| ());
+
     let handle = core.handle();
-    handle.spawn(future.map_err(|_| ()));
+    handle.spawn(future);
     loop {
-        core.turn(Some(Duration::from_millis(10)));
+        core.turn(Some(Duration::from_millis(0)));
 
         if gtk::events_pending() {
             gtk::main_iteration();
         }
-
     }
-    //core.run(sleep).unwrap();
 }
